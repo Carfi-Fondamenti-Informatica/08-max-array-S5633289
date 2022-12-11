1afa553e90dbb3ca9dd47bc5b5136d2dd673a769
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-11 21:30:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+//
+// Created by Davide on 02/12/2022.
+//
+#include "lib.h"
+#include <iostream>
 
+using namespace std;
+
+float maggiore(int n) {
+    float numeri[n];
+    for (int i = 0; i < n; i++) {
+        cout << "inserisci il numero:";
+        cin >> numeri[i];
+    }
+    float cont = numeri[0];
+    for (int i = 0; i < n; i++) {
+        if (numeri[i] > cont) {
+            cont = numeri[i];
+        }
+    }
+    return cont;
+}
