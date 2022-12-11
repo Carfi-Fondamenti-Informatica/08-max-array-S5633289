9c4d0fdf63f2a834f07060b8b07f72de4e99cc52
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-11 21:59:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,11 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "inserisci la dimensione del vettore:";
     cin >> n;
-    if (n > 0) {
-        float numeri[n];
-        for (int i = 0; i < n; i++) {
-            cout << "inserisci un numero:";
-            cin >> numeri[i];
-        }
-        cout << maggiore(numeri, n);
+    float numeri[n];
+    for (int i = 0; i < n; i++) {
+        cin >> numeri[i];
     }
+    cout << maggiore(numeri, n);
     return 0;
 }
