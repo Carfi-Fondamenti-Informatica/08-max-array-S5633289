1fa4ca7219d537093c3ce9359f554209175ba20e
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-11 21:55:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "lib.h"
+
 using namespace std;
 
 
 int main() {
     int n;
+    cout << "inserisci la dimensione del vettore:";
     cin >> n;
-    cout << maggiore(n);
-
+    if (n > 0) {
+        float numeri[n];
+        for (int i = 0; i < n; i++) {
+            cout << "inserisci un numero:";
+            cin >> numeri[i];
+        }
+        cout << maggiore(numeri, n);
+    }
     return 0;
 }
